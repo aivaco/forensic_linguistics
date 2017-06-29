2e663571979c520d8e585155a3c58f6c57994d27
Jose Jiménez <jocajime@gmail.com>
2017-06-29 00:06:51 -0600

    limpieza

--- src/com/forensic/Main.java
+++ src/com/forensic/Main.java
@@ -29,35 +29,5 @@ public class Main {
         for (TaggedWord taggedWord : a) {
             archivo.writeInFile(taggedWord.word());
         }
-
-
-//        if (args.length != 2) {
-//            log.info("usage: java TaggerDemo2 modelFile fileToTag");
-//            return;
-//        }
-
-//        MaxentTagger tagger = new MaxentTagger(".//models/wsj-0-18-left3words-distsim.tagger");
-//        TokenizerFactory<CoreLabel> ptbTokenizerFactory = PTBTokenizer.factory(new CoreLabelTokenFactory(),
-//                "untokenizable=noneKeep");
-//        BufferedReader r = new BufferedReader(new InputStreamReader(new FileInputStream(".//sample-input.txt"), "utf-8"));
-//        PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out, "utf-8"));
-//        DocumentPreprocessor documentPreprocessor = new DocumentPreprocessor(r);
-//        documentPreprocessor.setTokenizerFactory(ptbTokenizerFactory);
-//        for (List<HasWord> sentence : documentPreprocessor) {
-//            List<TaggedWord> tSentence = tagger.tagSentence(sentence);
-//            pw.println(SentenceUtils.listToString(tSentence, false));
-//        }
-//
-//        // print the adjectives in one more sentence. This shows how to get at words and tags in a tagged sentence.
-//        List<HasWord> sent = SentenceUtils.toWordList("The", "slimy", "slug", "crawled", "over", "the", "long", ",", "green", "grass", ".");
-//        List<TaggedWord> taggedSent = tagger.tagSentence(sent);
-//        for (TaggedWord tw : taggedSent) {
-//            if (tw.tag().startsWith("JJ")) {
-//                pw.println(tw.word());
-//            }
-//        }
-//
-//        pw.close();
-//    }
     }
 }
